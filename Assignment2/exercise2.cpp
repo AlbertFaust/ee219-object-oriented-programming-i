d7dad5b3dd6cfaad2909b88d6f576c9f4abd3f21
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-10-19 16:13:56 +0100

    Create exercise2.cpp

---
+++ Assignment2/exercise2.cpp
@@ -0,0 +1,29 @@
+/*Assignment 2 - Exercise 2
+Write a function of the form int countOdds(int[], int) that returns the number of odd numbered ints in a given array, when passed the array and the number of elements in the array. 
+Remember the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+	countOdds({2, 1, 2, 3, 4},5) → 2
+	countOdds({2, 2, 0},3) → 0
+	countOdds({1, 3, 5},3) → 3
+*/
+#include <iostream>
+using namespace std;
+
+int countOdds(int nums[], int size)
+{
+    int oddNums;
+    for(int i = 0; i < size; i++)
+    {
+        if(nums[i] % 2 == 1)
+        {
+            oddNums++;
+        }
+    }
+    return oddNums;
+}
+
+int main()
+{
+    int nums[5] = {1, 2, 3, 4, 5};
+    int size = 5;
+    cout <<  countOdds(nums, size) << endl;
+}
