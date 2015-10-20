b2c295fd4333de3d5d534b3ecb201719af6b9f71
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-10-20 21:22:05 +0100

    Added line to question

--- Assignment2/exercise2.cpp
+++ Assignment2/exercise2.cpp
@@ -4,6 +4,7 @@ Remember the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
 	countOdds({2, 1, 2, 3, 4},5) → 2
 	countOdds({2, 2, 0},3) → 0
 	countOdds({1, 3, 5},3) → 3
+Please note: the example code is pseudo code and is illustrative - you should not pass values in this way.
 */
 #include <iostream>
 using namespace std;
