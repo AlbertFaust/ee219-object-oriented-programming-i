cc791c3ca78e1ffda290990483140fc03d891714
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-10-28 23:03:09 +0000

    Create exercise8.cpp

---
+++ Assignment3/exercise8.cpp
@@ -0,0 +1,59 @@
+/*Assignment 3 - Exercise 8
+Add a "getter" and "setter" to the Vehicle class to get and set the colour of the Vehicle.
+*/
+#include <iostream>
+using namespace std;
+
+class Vehicle{
+	private:
+		string colour;
+		string make;
+		string model;
+		static int vehNum;
+	public:
+		virtual void display();
+		Vehicle(string, string, string);
+		~Vehicle();
+		string getColour();
+		void setColour(string);
+};
+void Vehicle::display(){
+
+    cout << "The vehicle's colour is " << colour << endl;
+    cout << "The vehicle's make is " << make << endl;
+    cout << "The vehicle's model is " << model << endl;
+}
+Vehicle::Vehicle(string col, string makeVeh, string modelVeh){
+	cout << "A Vehicle has just been created" << endl;
+	colour = col;
+	make = makeVeh;
+	model = modelVeh;
+}
+string Vehicle::getColour(){
+	return colour;
+}
+void Vehicle::setColour(string col){
+	colour = col;
+}
+Vehicle::~Vehicle(void){cout << "A Vehicle has just been destroyed" << endl;}
+
+class Car : public Vehicle{
+	private:
+		int seats;
+		int doors;
+	public:
+		Car(string col, string makeCar, string modelCar, int seatsCar, int doorsCar):Vehicle(col, makeCar, modelCar){
+			cout << "A Vehicle has just been created" << endl;
+			seats = seatsCar;
+			doors = doorsCar;
+		}
+		~Car();
+};
+Car::~Car(void){cout << "A Car has just been destroyed" << endl;}
+
+int main()
+{
+	Vehicle robsVehicle = Vehicle("Black", "Porsche", "911");
+	Car robsCar = Car("Red", "Toyota", "Yaris", 5, 5);
+	robsVehicle.setColour("white");
+}
